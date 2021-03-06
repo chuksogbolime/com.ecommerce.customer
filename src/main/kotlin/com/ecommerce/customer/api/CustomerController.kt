e123fa71f348a7c432b33c1b124ed18f4af11c5b
chuksogbolime <chuksogbolime@gmail.com>
2021-03-06 03:00:20 +0100

    updated customer controller with RESTful responses

--- src/main/kotlin/com/ecommerce/customer/api/CustomerController.kt
+++ src/main/kotlin/com/ecommerce/customer/api/CustomerController.kt
@@ -4,34 +4,38 @@ import com.ecommerce.customer.core.extensions.toCustomerDTO
 import com.ecommerce.customer.core.interfaces.ICustomerCommandService
 import com.ecommerce.customer.core.interfaces.ICustomerQueryService
 import com.ecommerce.customer.core.models.api.requests.CustomerApiRequest
+import org.springframework.http.HttpStatus
 
 
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import java.net.URI
 
 @RestController
 @RequestMapping("/api/customer")
 class CustomerController(private val customerQueryService: ICustomerQueryService,
                          private val customerCommandService: ICustomerCommandService
 ){
-
+    val rootLink = "/api/customer"
     @GetMapping
     fun getCustomers():ResponseEntity<Any>
     {
         val result = customerQueryService.getCustomers().toList()
-        //return ResponseEntity.ok(result)
-        return if(result.isNotEmpty()) ResponseEntity.ok(result) else ResponseEntity.badRequest().body("Could not fetch data")
+        return if(result.isNotEmpty()) ResponseEntity.ok(result) else ResponseEntity<Any>(HttpStatus.NO_CONTENT)
     }
 
 
     @PostMapping
     fun addCustomer(@Validated @RequestBody customer: CustomerApiRequest):ResponseEntity<Any>{
         val result=customerCommandService.addCustomer(customer.toCustomerDTO())
-        return if(result.second)
-            ResponseEntity.ok(result.first!!)
+
+        return if(result.second){
+            val urlString = "$rootLink/${result.first?.id}"
+            ResponseEntity.created(URI.create(urlString)).body(result.first!!)
+        }
         else
-            ResponseEntity.badRequest().body("Error saving customer")
+            ResponseEntity.badRequest().body(null)
     }
 
     @PutMapping("/{id}")
@@ -39,22 +43,23 @@ class CustomerController(private val customerQueryService: ICustomerQueryService
         @Validated @RequestBody customer: CustomerApiRequest):ResponseEntity<Any>{
 
         val result=customerCommandService.updateCustomer(id, customer.toCustomerDTO())
+
         return if(result.second)
-            ResponseEntity.ok(result.first!!)
+            ResponseEntity<Any>(HttpStatus.NO_CONTENT)
         else
-            ResponseEntity.badRequest().body("Error updating customer")
+            ResponseEntity.badRequest().body(null)
     }
 
     @GetMapping("/{id}")
     fun getCustomerById(@PathVariable(value = "id") id: Long):ResponseEntity<Any>{
         val result=customerQueryService.getCustomerById(id)
-        return if(result !=null) ResponseEntity.ok(result) else ResponseEntity.badRequest().body("Customer with Id:$id does not exist")
+        return if(result !=null) ResponseEntity.ok(result) else ResponseEntity.notFound().build()
     }
 
     @DeleteMapping("/{id}")
     fun deleteCustomer(@PathVariable(value = "id") id: Long):ResponseEntity<Any>{
         val result=customerCommandService.deleteCustomer(id)
-        return if(result.first) ResponseEntity.ok(result.second) else ResponseEntity.badRequest().body("Customer with Id:$id does not exist")
+        return if(result.first) ResponseEntity<Any>(HttpStatus.OK) else ResponseEntity.notFound().build()
     }
 
 }
